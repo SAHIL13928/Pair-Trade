67194b95b531825805f11840a810c1a16bdf7d6b
SAHIL13928 <148043150+SAHIL13928@users.noreply.github.com>
2024-09-11 00:41:51 +0530

    .

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,10 +5,10 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons';
 const Header = () => {
   return (
     <div className="flex items-center justify-center bg-gray-100 p-4 shadow">
-      <div className="relative mr-auto w-full max-w-3xl">  {/* Increased width for a longer search bar */}
+      <div className="relative mr-auto w-full max-w-3xl">  
         <input 
           type="text" 
-          className="w-full p-4 pl-24 rounded-full border border-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 bg-white text-black"
+          className="w-full p-4 pl-24 rounded-full border border-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-600 bg-white text-black"
           placeholder="Search..." 
         />
         <button className="absolute left-2 top-1/2 transform -translate-y-1/2 bg-blue-600 rounded-full px-6  py-3 flex items-center justify-center">
